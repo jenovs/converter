bd2de67a78a7b511ce589e4490ec405801319255
Viktors Jenovs <jviktors@gmail.com>
2018-02-21 20:34:46 +0200

    Create basic Container structure

---
+++ app/components/Container/styles.js
@@ -0,0 +1,10 @@
+import stylesheet from 'react-native-extended-stylesheet';
+
+export default stylesheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '$primaryBlue',
+  },
+});
--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import stylesheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
+
+stylesheet.build({
+  $primaryBlue: '#4f6d7a',
+});
 
 export default () => <Home />;
